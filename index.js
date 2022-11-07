8a0ab1460128db93f098b129c6504a580ea37335
FiLLkrd <ma.antonoff2012@yandex.ru>
2022-11-08 00:05:24 +0300

    Доделана верстка 1 экрана, добавлен эффект наведения на иконки, прописана логика выбора кнопок и выпадающего списка

---
+++ index.js
@@ -0,0 +1,76 @@
+const dom = {
+    selectbox: document.getElementById('selectbox'),
+    rooms: document.getElementById('rooms'),
+    selectboxList: document.querySelector('.selectbox__list')
+}
+
+const rooms = {
+    all: 'Все комнаты',
+    livingroom: 'Зал',
+    bedroom: 'Спальня',
+    kitchen: 'Кухня',
+    bathroom: 'Ванная',
+    studio: 'Кабинет',
+    washingroom: 'Уборная'
+}
+
+console.log(dom)
+
+//Выпадающий список
+
+dom.selectbox.querySelector('.selectbox__selected').onclick = (event) => {
+    dom.selectbox.classList.toggle('open')
+}
+
+document.body.onclick = (event) => {
+    const { target } = event
+    if (
+        !target.matches('.selectbox')
+        && !target.parentElement.matches('.selectbox')
+        && !target.parentElement.parentElement.matches('.selectbox')
+        ){
+            dom.selectbox.classList.remove('open');
+        }
+}
+
+dom.selectboxList.onclick = (event) => {
+    const { target } = event
+    if (target.matches('.selectbox__item')){
+        const { room } = target.dataset
+        const selectedItem = dom.selectboxList.querySelector('.selected')
+        selectedItem.classList.remove('selected')
+        target.classList.add('selected')
+        dom.selectbox.classList.remove('open')
+        selectRoom(room)
+    }
+}
+
+//Выбор комнаты
+
+function selectRoom (room) {
+    const selectedRoom = dom.rooms.querySelector('.selected');
+    
+    if (selectedRoom) {
+        selectedRoom.classList.remove('selected')
+        
+    }
+    if (room != 'all') {
+       const newSelectedRoom = dom.rooms.querySelector(`[data-room=${room}]`)
+       newSelectedRoom.classList.add('selected')
+    }
+    const selectedSelectboxRoom = dom.selectbox.querySelector('.selectbox__item.selected')
+    selectedSelectboxRoom.classList.remove('selected')
+    const newSelectedItem = dom.selectbox.querySelector(`[data-room=${room}]`)
+    newSelectedItem.classList.add('selected')
+    const selectboxSelected = dom.selectbox.querySelector('.selectbox__selected span')
+    selectboxSelected.innerText = rooms[room]
+}
+
+//Клик по элементу комнаты
+
+dom.rooms.querySelectorAll('.room').forEach(room => {
+    room.onclick = (event) => {
+        const value = room.dataset.room
+        selectRoom(value)
+    }
+})
